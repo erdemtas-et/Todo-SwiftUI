d469536a562676b0f2efc18c2921adb666e27ca0
erdemtas-et <erdemtas.dev@gmail.com>
2023-05-02 10:31:00 +0300

    Initial Commit

---
+++ Todo-SwiftUI/Todo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_SwiftUIApp.swift
+//  Todo-SwiftUI
+//
+//  Created by Erdem Tas on 2.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Todo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
